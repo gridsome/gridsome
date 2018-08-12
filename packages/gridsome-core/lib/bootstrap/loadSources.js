fc3c77e19e9f770a47369b67d81861c523d5f06c
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-08-12 18:13:56 +0200

    send query to front-end for re-fetch on change

--- packages/gridsome-core/lib/bootstrap/loadSources.js
+++ packages/gridsome-core/lib/bootstrap/loadSources.js
@@ -8,18 +8,26 @@ module.exports = async service => {
       const source = new SourceAPI(service, plugin)
       await plugin.api.initSource(source)
 
-      source.on('addPage', page => generateRoutes(service))
-      source.on('removePage', () => generateRoutes(service))
+      if (process.env.NODE_ENV === 'development') {
+        source.on('addPage', page => generateRoutes(service))
+        source.on('removePage', () => generateRoutes(service))
 
-      source.on('updatePage', async (page, oldPage) => {
-        const { pageQuery: { paginate: oldPaginate }} = oldPage
-        const { pageQuery: { paginate }} = page
+        source.on('updatePage', async (page, oldPage) => {
+          const { pageQuery: { paginate: oldPaginate }} = oldPage
+          const { pageQuery: { paginate }} = page
 
-        // regenerate route.js whenever paging options changes
-        if (paginate.collection !== oldPaginate.collection) {
-          await generateRoutes(service)
-        }
-      })
+          // regenerate route.js whenever paging options changes
+          if (paginate.collection !== oldPaginate.collection) {
+            await generateRoutes(service)
+          }
+
+          // send query to front-end for re-fetch
+          service.broadcast({
+            query: page.pageQuery.content,
+            file: page.file
+          })
+        })
+      }
 
       service.sources.push({ plugin, source })
     }
