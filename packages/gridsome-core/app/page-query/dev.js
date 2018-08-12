8f2d342b70ad1d2a160e1abffc51b3a208034db7
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-08-12 22:00:44 +0200

    improve dev query fetcher

--- packages/gridsome-core/app/page-query/dev.js
+++ packages/gridsome-core/app/page-query/dev.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
-import sock from '../sock'
+import sock from './sock'
 import fetch from './fetch'
 
-const merge = Vue.config.optionMergeStrategies
+const active = {}
+
+sock.onmessage = message => {
+  const { query, file } = JSON.parse(message.data)
+
+  if (active.hasOwnProperty(file)) {
+    active[file].options.__pageQuery = query
+  }
+
+  for (const file in active) {
+    const { options, vm } = active[file]
+    fetch(options, vm.$route)
+  }
+}
 
 export default ({ options }) => {
   if (options.__pageQueryDev) return
 
+  const merge = Vue.config.optionMergeStrategies
+
   options.mounted = merge.mounted([function () {
-    sock.onmessage = message => {
-      const { query, file } = JSON.parse(message.data)
-      if (file !== this.$options.__file) return
-      options.__pageQuery = query
-      fetch(options, this.$route)
-    }
+    active[options.__file] = { options, vm: this }
   }], options.mounted)
 
   options.beforeDestroy = merge.beforeDestroy([function () {
-    sock.onmessage = null
+    delete active[options.__file]
   }], options.beforeDestroy)
 
   options.__pageQueryDev = true
