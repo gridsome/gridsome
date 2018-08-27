5bb35429961a0d20e57f22ec31123db624db5f81
Tommy Vedvik <tommyvedvik@gmail.com>
2018-08-27 10:11:03 +0200

    alt to images
    Add a automatic alt fallback system also maybe? if its possible to extract meta from images

--- gridsome/app/components/Image.js
+++ gridsome/app/components/Image.js
@@ -3,13 +3,15 @@ export default {
 
   props: {
     src: { type: String },
-    width: { type: String }
+    width: { type: String },
+    alt: {type: String}
   },
 
   render: (h, ctx) => {
     return h('img', {
       ...ctx.data,
       attrs: {
+        alt: ctx.props.alt,
         src: ctx.props.src
       }
     })
