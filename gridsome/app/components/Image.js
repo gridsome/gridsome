a867567b2ff866e9936224c965bc03eb9c446917
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-08-31 23:11:21 +0200

    set alt attr on noscript image

--- gridsome/app/components/Image.js
+++ gridsome/app/components/Image.js
@@ -47,7 +47,7 @@ export default {
         h('img', {
           ...data,
           class: noscriptClass,
-          attrs: { src }
+          attrs: { src, alt: props.alt }
         })
       ]))
     }
