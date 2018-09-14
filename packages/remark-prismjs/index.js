20cb03e80bcda5f3595e14cd129a8105d6bdc528
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-09-14 13:46:04 +0200

    highlight graphql in html

--- packages/remark-prismjs/index.js
+++ packages/remark-prismjs/index.js
@@ -1,33 +1,41 @@
-const prism = require('prismjs')
+const Prism = require('prismjs')
 const escapeHtml = require('escape-html')
 const visit = require('unist-util-visit')
+const loadLanguages = require('prismjs/components/index')
 
 module.exports = options => {
+  loadLanguages(['graphql'])
+
+  // highlight for graphql and static-query tags in html
+  Prism.languages.html.graphql = {
+    pattern: /(<(graphql|static-query)[\s\S]*?>)[\s\S]*?(?=<\/(graphql|static-query)>)/i,
+    inside: Prism.languages.graphql,
+    lookbehind: true,
+    greedy: true
+  }
+
   return tree => {
     visit(tree, 'code', node => {
-      const { value, lang } = node
-      const code = highlight(value, lang)
-
       node.type = 'html'
-      node.value = `<pre class="language-${lang}"><code>${code}</code></pre>`
+      node.value = highlight(node, 'pre')
     })
 
     visit(tree, 'inlineCode', node => {
-      const { value, lang } = node
-      const code = highlight(value, lang)
-
       node.type = 'html'
-      node.value = `<code class="language-${lang}">${code}</code>`
+      node.value = highlight(node, 'code')
     })
   }
 }
 
-function highlight (value, lang) {
-  let code = prism.languages[lang]
-    ? prism.highlight(value, prism.languages[lang], lang)
+function highlight ({ value, lang }, tag = 'pre') {
+  let code = Prism.languages.hasOwnProperty(lang)
+    ? Prism.highlight(value, Prism.languages[lang], lang)
     : value
 
-  if (!lang) code = escapeHtml(code)
+  if (!lang) {
+    lang = 'text'
+    code = escapeHtml(code)
+  }
 
-  return code
+  return `<${tag} class="language-${lang}">${code}</${tag}>`
 }
