dfeeb90022bfefb0df7dfe62228e45d8bb4e0505
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-09-24 01:35:25 +0200

    fix(image): re-observe after hot-reload

--- gridsome/app/components/Image.js
+++ gridsome/app/components/Image.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 
+const supportsIntersectionObserver = (
+  'document' in global &&
+  'IntersectionObserver' in global &&
+  'IntersectionObserverEntry' in global &&
+  'intersectionRatio' in global.IntersectionObserverEntry.prototype
+)
+
+const observer = supportsIntersectionObserver
+  ? createObserver()
+  : null
+
+let uid = 0
+
 export default {
   functional: true,
 
@@ -20,19 +33,28 @@ export default {
   },
 
   render: (h, { data, props, parent }) => {
+    const isDev = process.env.NODE_ENV === 'development'
     const { src, srcset, sizes, size, dataUri } = props.src
     const isLazy = typeof props.immediate === 'undefined'
     const classNames = (data.class || []).concat(['g-image'])
     const noscriptClass = classNames.slice()
     const res = []
 
+    const ref = data.ref || `__image_${uid++}`
+    const key = data.key || isDev ? ref : undefined
+
+    // we set a key to force update image after hot-reload because
+    // the html attributes doesn't re-render after deletion
+
     res.push(h('img', {
       ...data,
+      ref,
+      key,
       class: classNames,
       attrs: {
-        'src': dataUri,
-        'alt': props.alt,
-        'width': size.width,
+        src: dataUri,
+        alt: props.alt,
+        width: size.width,
         [`${isLazy ? 'data-' : ''}srcset`]: srcset.join(', '),
         [`${isLazy ? 'data-' : ''}sizes`]: sizes,
         [`${isLazy ? 'data-' : ''}src`]: src
@@ -40,12 +62,25 @@ export default {
     }))
 
     if (isLazy) {
+      const onMount = el => {
+        if (!el) return
+        else if (!observer) loadImage(el)
+        else observer.observe(el)
+      }
+
+      const onDestroy = el => {
+        if (el && observer) observer.unobserve(el)
+      }
+
       classNames.push('g-image--lazy')
       noscriptClass.push('g-image--loaded')
 
+      parent.$once('hook:mounted', () => onMount(parent.$refs[ref]))
+      parent.$once('hook:updated', () => onMount(parent.$refs[ref]))
+      parent.$once('hook:beforeDestroy', () => onDestroy(parent.$refs[ref]))
+
       res.push(h('noscript', null, [
         h('img', {
-          ...data,
           class: noscriptClass,
           attrs: {
             src,
@@ -59,17 +94,6 @@ export default {
     return res
   }
 }
-
-const supportsIntersectionObserver = (
-  'document' in global &&
-  'IntersectionObserver' in global &&
-  'IntersectionObserverEntry' in global &&
-  'intersectionRatio' in global.IntersectionObserverEntry.prototype
-)
-
-const observer = supportsIntersectionObserver
-  ? createObserver()
-  : null
 
 export function initIntersectionObserver (router) {
   if (observer) {
@@ -88,16 +112,16 @@ export function observe (selector = '[data-src]', context = document) {
   const images = context.querySelectorAll(selector)
 
   if (observer) {
-    images.forEach(el => observer.observe(el))
+    images.forEach(el => !el.__vue__ && observer.observe(el))
   } else {
-    Array.from(images).forEach(el => loadImage(el))
+    Array.from(images).forEach(el => !el.__vue__ && loadImage(el))
   }
 }
 
 export function unobserve (selector = '[data-src]', context = document) {
   if (observer) {
     context.querySelectorAll(selector).forEach(el => {
-      observer.unobserve(el)
+      if (!el.__vue__) observer.unobserve(el)
     })
   }
 }
