8b9f256204c333eeca1f5d04ad214081cd8cc7f1
Marcel Behrmann <mail@marcelbehrmann.de>
2018-11-09 13:15:44 +0100

    Remove file check

--- gridsome/lib/graphql/schema/types/image.js
+++ gridsome/lib/graphql/schema/types/image.js
@@ -5,39 +5,27 @@ const { GraphQLJSON } = require('../../graphql')
 const { SUPPORTED_IMAGE_TYPES } = require('../../../utils/constants')
 
 exports.isImage = value => {
-    const extName = path.extname(value)
+  const extName = path.extname(value)
 
-    if (!(typeof value === 'string' || value instanceof String)) {
-        return false
-    }
+  if (!(typeof value === 'string' || value instanceof String)) {
+    return false
+  }
 
-    if (!SUPPORTED_IMAGE_TYPES.includes(extName)) {
-        return false
-    }
+  if (!SUPPORTED_IMAGE_TYPES.includes(extName.toLowerCase())) {
+    return false
+  }
 
-    if(path.basename(value) === value) {
-        return false
-    }
+  if (path.basename(value) === value) {
+    return false
+  }
 
-    return true
+  return true
 }
 
 exports.imageType = {
-    type: GraphQLJSON,
-    args: {},
-    resolve: async (fields, {}, context, { fieldName }) => {
-        let file = fields[fieldName]
-        
-        if (file.startsWith(path.sep)) {
-            file = `.${file}`
-        }
-
-        if (!fs.existsSync(file)) {
-            return fields[fieldName]
-        }
-
-        const result = await context.queue.add(file);
-        
-        return result;
-    }
+  type: GraphQLJSON,
+  args: {},
+  resolve: async (fields, { }, context, { fieldName }) => {
+    return await context.queue.add(fields[fieldName]);;
+  }
 }
