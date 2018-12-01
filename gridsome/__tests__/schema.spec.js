555535451e1975f8ef055969d0539c37d137522f
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-12-01 19:33:48 +0100

    fix(graphql): transform nested invalid field names

--- gridsome/__tests__/schema.spec.js
+++ gridsome/__tests__/schema.spec.js
@@ -377,6 +377,85 @@ test('should get values from object fields', async () => {
   expect(data.testPost.myObject.otherObject.value).toEqual('test2')
 })
 
+test('should convert keys to valid field names', async () => {
+  const contentType = api.store.addContentType({
+    typeName: 'TestPost'
+  })
+
+  const node = contentType.addNode({
+    id: '1',
+    fields: {
+      'my-object': {
+        '2value': 'test',
+        ':value': 'test',
+        'test:value': 'test',
+        'other-object': {
+          value: 'test'
+        }
+      }
+    }
+  })
+
+  const { errors, data } = await createSchemaAndExecute(`{
+    testPost (id: "1") {
+      myObject {
+        _2value
+        value
+        testValue
+        otherObject {
+          value
+        }
+      }
+    }
+  }`)
+
+  expect(errors).toBeUndefined()
+  expect(data.testPost).toMatchObject({
+    myObject: {
+      _2value: 'test',
+      value: 'test',
+      testValue: 'test',
+      otherObject: {
+        value: 'test'
+      }
+    }
+  })
+})
+
+test('preserve internal custom fields', async () => {
+  const contentType = api.store.addContentType({
+    typeName: 'TestPost'
+  })
+
+  const node = contentType.addNode({
+    id: '1',
+    fields: {
+      __hidden: true,
+      nested: {
+        value: 'test',
+        '__nested-hidden': true
+      }
+    }
+  })
+
+  const { errors, data } = await createSchemaAndExecute(`{
+    testPost (id: "1") {
+      __hidden
+      nested {
+        value
+        __nested_hidden
+      }
+    }
+  }`)
+
+  expect(data).toBeUndefined()
+  expect(errors).toHaveLength(2)
+  expect(errors[0].message).toEqual('Cannot query field "__hidden" on type "TestPost".')
+  expect(errors[1].message).toEqual('Cannot query field "__nested_hidden" on type "TestPostNested".')
+  expect(node.fields.__hidden).toBeTruthy()
+  expect(node.fields.nested['__nested-hidden']).toBeTruthy()
+})
+
 test('should format dates from schema', async () => {
   const contentType = api.store.addContentType({
     typeName: 'TestPostDate'
