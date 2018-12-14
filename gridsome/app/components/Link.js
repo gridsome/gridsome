a6565cab3b171914f6d6a0919ce8bfcc64045691
David Ryan <reachdavidryan@gmail.com>
2018-12-14 10:13:29 -0700

    set default props for link active states

--- gridsome/app/components/Link.js
+++ gridsome/app/components/Link.js
@@ -1,15 +1,15 @@
 /* global GRIDSOME_MODE */
 
-import router from '../router'
-import caniuse from '../utils/caniuse'
-import { stripPathPrefix } from '../utils/helpers'
-import { createObserver } from '../utils/intersectionObserver'
+import router from "../router";
+import caniuse from "../utils/caniuse";
+import { stripPathPrefix } from "../utils/helpers";
+import { createObserver } from "../utils/intersectionObserver";
 
 const observer = caniuse.IntersectionObserver
   ? createObserver(intersectionHandler)
-  : null
+  : null;
 
-let uid = 0
+let uid = 0;
 
 export default {
   functional: true,
@@ -17,87 +17,92 @@ export default {
   props: {
     to: { type: [Object, String] },
     page: { type: Number },
-    activeClass: { type: String },
-    exactActiveClass: { type: String }
+    activeClass: { type: String, default: "active" },
+    exactActiveClass: { type: String, default: "active--exact" }
   },
 
   render: (h, { data, props, parent, children, ...res }) => {
-    if (props.to && props.to.type === 'file') {
-      data.attrs.href = props.to.src
-      return h('a', data, children)
+    if (props.to && props.to.type === "file") {
+      data.attrs.href = props.to.src;
+      return h("a", data, children);
     }
 
-    const ref = data.ref || `__link_${uid++}`
-    const to = typeof props.to === 'string'
-      ? { path: props.to, params: {}}
-      : { params: {}, ...props.to }
-    let activeClass = typeof props.activeClass === 'string' ? props.activeClass : 'active'
-    let exactActiveClass = typeof props.exactActiveClass === 'string' ? props.exactActiveClass : 'active--exact'
+    const ref = data.ref || `__link_${uid++}`;
+    const to =
+      typeof props.to === "string"
+        ? { path: props.to, params: {} }
+        : { params: {}, ...props.to };
 
     if (props.page) {
-      to.params.page = props.page > 1 ? props.page : null
-      data.attrs.exact = true
+      to.params.page = props.page > 1 ? props.page : null;
+      data.attrs.exact = true;
     }
 
-    if (GRIDSOME_MODE === 'static' && process.isClient) {
+    if (GRIDSOME_MODE === "static" && process.isClient) {
       const onMount = vm => {
-        if (vm && observer) observer.observe(vm.$el)
-      }
+        if (vm && observer) observer.observe(vm.$el);
+      };
 
       const onDestroy = vm => {
-        if (vm && observer) observer.unobserve(vm.$el)
-      }
+        if (vm && observer) observer.unobserve(vm.$el);
+      };
 
-      parent.$once('hook:mounted', () => onMount(parent.$refs[ref]))
-      parent.$once('hook:updated', () => onMount(parent.$refs[ref]))
-      parent.$once('hook:beforeDestroy', () => onDestroy(parent.$refs[ref]))
+      parent.$once("hook:mounted", () => onMount(parent.$refs[ref]));
+      parent.$once("hook:updated", () => onMount(parent.$refs[ref]));
+      parent.$once("hook:beforeDestroy", () => onDestroy(parent.$refs[ref]));
     }
 
-    return h('router-link', {
-      ...data,
-      ref,
-      attrs: {
-        to,
-        activeClass: activeClass,
-        exactActiveClass: exactActiveClass,
-        ...data.attrs
+    return h(
+      "router-link",
+      {
+        ...data,
+        ref,
+        attrs: {
+          to,
+          activeClass: activeClass,
+          exactActiveClass: exactActiveClass,
+          ...data.attrs
+        },
+        domProps: {
+          __gLink__: true
+        }
       },
-      domProps: {
-        __gLink__: true
-      }
-    }, children)
+      children
+    );
   }
-}
+};
 
-const isPreloaded = {}
+const isPreloaded = {};
 
-function intersectionHandler ({ intersectionRatio, target }) {
+function intersectionHandler({ intersectionRatio, target }) {
   if (process.isClient) {
     if (intersectionRatio > 0) {
-      observer.unobserve(target)
+      observer.unobserve(target);
 
       if (document.location.hostname === target.hostname) {
-        if (isPreloaded[target.pathname]) return
-        else isPreloaded[target.pathname] = true
+        if (isPreloaded[target.pathname]) return;
+        else isPreloaded[target.pathname] = true;
 
-        const path = stripPathPrefix(target.pathname)
-        const { route } = router.resolve({ path })
+        const path = stripPathPrefix(target.pathname);
+        const { route } = router.resolve({ path });
 
         const fetchComponentData = options => {
           setTimeout(() => {
-            import(/* webpackChunkName: "page-query" */ '../page-query/fetch').then(m => {
-              m.default(route, options.__pageQuery)
-            })
-          }, 250)
-        }
+            import(/* webpackChunkName: "page-query" */ "../page-query/fetch").then(
+              m => {
+                m.default(route, options.__pageQuery);
+              }
+            );
+          }, 250);
+        };
 
         if (route.meta.data && route.matched.length) {
-          const options = route.matched[0].components.default
+          const options = route.matched[0].components.default;
 
-          if (typeof options === 'function') {
-            options().then(m => fetchComponentData(m.default))
+          if (typeof options === "function") {
+            options().then(m => fetchComponentData(m.default));
           } else {
-            fetchComponentData(options)
+            fetchComponentData(options);
           }
         }
       }
