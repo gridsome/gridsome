5e66161cae50c396f5e925569b01a652a70fc1a5
Tommy Vedvik <tommyvedvik@gmail.com>
2019-01-04 14:21:48 +0100

    fix indenting

--- packages/plugin-example/gridsome.server.js
+++ packages/plugin-example/gridsome.server.js
@@ -15,27 +15,26 @@ class ExamplePlugin {
 
   constructor (api, options) {
 
-		api.loadSource(store => {
+    api.loadSource(store => {
      	
-     	// Creates a content type
-     	const posts = store.addContentType({
-		    typeName: options.typeName,
-		    route: options.route
-		  })
+      	// Creates a content type
+      const posts = store.addContentType({
+        typeName: options.typeName,
+        route: options.route
+      })
 
-     	// Add content nodes
+      	// Add content nodes
       posts.addNode({
-		    title: 'Example post',
-		    content: 'Lorem ipsum dolor sit amet, consectetur...',
-	  	})
+        title: 'Example post',
+        content: 'Lorem ipsum dolor sit amet, consectetur...',
+      })
 
-	  	posts.addNode({
-		    title: 'Another post',
-		    content: 'Lorem ipsum dolor sit amet, consectetur...',
-	  	})
+      posts.addNode({
+        title: 'Another post',
+        content: 'Lorem ipsum dolor sit amet, consectetur...',
+      })
 
   	})
   }
 }
-
 module.exports = ExamplePlugin
