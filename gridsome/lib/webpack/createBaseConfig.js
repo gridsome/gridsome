01316775e4f89607d0427c374fef4bd31d661078
isoppp <prog@d2deck.com>
2019-01-17 15:29:04 +0900

    feat(webpack): change the name prefix to be automatically set from GRIDSOME_ENV to GRIDSOME_.

--- gridsome/lib/webpack/createBaseConfig.js
+++ gridsome/lib/webpack/createBaseConfig.js
@@ -327,7 +327,7 @@ module.exports = (app, { isProd, isServer }) => {
     }
 
     // merge variables start with GRIDSOME_ENV to config.env
-    const gridsomeEnv = pick(process.env, Object.keys(process.env).filter(key => key.startsWith('GRIDSOME_ENV')))
+    const gridsomeEnv = pick(process.env, Object.keys(process.env).filter(key => key.startsWith('GRIDSOME_')))
     const mergeEnv = Object.entries(Object.assign({}, projectConfig.env || {}, gridsomeEnv))
       .reduce((acc, [key, value]) => {
         acc[`process.env.${key}`] = ['boolean', 'number'].includes(typeof value) ? value : JSON.stringify(value)
