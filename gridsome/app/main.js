bb62605180bcf3ab4d0533833825329bc8d2437c
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-01-29 21:59:51 +0100

    fix(build): throw error if something fails in main.js

--- gridsome/app/main.js
+++ gridsome/app/main.js
@@ -3,7 +3,11 @@ let main
 try {
   main = require('~/main').default
 } catch (err) {
-  main = function () {}
+  if (err.code === 'MODULE_NOT_FOUND') {
+    main = function () {}
+  } else {
+    throw err
+  }
 }
 
 export default main
