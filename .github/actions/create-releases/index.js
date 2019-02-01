02dc0d29a203f4323d88f917efc32b5674e127f5
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-02-01 12:00:56 +0100

    find all change logs and list them

--- .github/actions/create-releases/index.js
+++ .github/actions/create-releases/index.js
@@ -1,8 +1,24 @@
-const { GITHUB_EVENT_PATH, GITHUB_WORKSPACE } = process.env
+const glob = require('globby')
 
-const payload = GITHUB_EVENT_PATH ? require(GITHUB_EVENT_PATH) : {}
+run()
 
-console.log('Create Releases', {
-  workspace: GITHUB_WORKSPACE,
-  payload
-})
+async function run () {
+  const payload = process.env.GITHUB_EVENT_PATH
+    ? require(process.env.GITHUB_EVENT_PATH)
+    : {}
+
+  const isReleasing = payload.commits.some(commit => {
+    return commit.message === 'chore(release): publish'
+  })
+
+  if (!isReleasing) {
+    console.log('Nothing to release')
+  }
+
+  const changelogs = await glob('**/*/CHANGELOG.md', {
+    cwd: process.env.GITHUB_WORKSPACE,
+    absolute: true
+  })
+
+  console.log(changelogs)
+}
