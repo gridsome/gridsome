1ee57fe1e56d8e30bf229f4a3a178d9abf15f92d
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-02-23 19:36:40 +0100

    fix(build): render pagination correctly for root path (#218)

---
+++ gridsome/lib/__tests__/__fixtures__/project-blog/src/templates/Post.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <h1 v-html="$page.post.title"/>
+  </Layout>
+</template>
+
+<page-query>
+query Post ($id: String!) {
+  post (id: $id) {
+    title
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo () {
+    return {
+      title: this.$page.post.title
+    }
+  }
+}
+</script>
