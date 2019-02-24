b3b7addb5c32b81a038159b1138337fd2828e611
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-02-24 18:35:55 +0100

    fix(build): fail if path resolves to 404 (#218)

--- gridsome/app/entry.server.js
+++ gridsome/app/entry.server.js
@@ -1,17 +1,24 @@
 import createApp, { runMain } from './app'
 
+const NOT_FOUND_URL = '/404'
+const NOT_FOUND_NAME = '404'
+
 runMain()
 
 export default context => new Promise((resolve, reject) => {
-  const { app, router } = createApp()
-  let { url } = context
+  const location = context.url === NOT_FOUND_URL
+    ? { name: NOT_FOUND_NAME }
+    : { path: context.url }
 
-  if (url === '/404') {
-    url = { name: '404' }
+  const { app, router } = createApp()
+  const { name } = router.resolve(location).route
+
+  if (location.name !== NOT_FOUND_NAME && name === NOT_FOUND_NAME) {
+    return reject(new Error(`Could not resolve ${context.url}`))
   }
   
   context.meta = app.$meta()
-  router.push(url)
+  router.push(location)
 
   router.onReady(() => {
     resolve(app)
