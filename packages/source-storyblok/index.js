b54b3022bed74519f07bf542424d29baed3a7c78
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-04-18 23:08:11 +0200

    various changes

--- packages/source-storyblok/index.js
+++ packages/source-storyblok/index.js
@@ -1,27 +1,87 @@
-const StoryblokClient = require('storyblok-js-client')
+class StoryblokSource {
+  defaultOptions () {
+    return {
+      typeName: 'Storyblok',
+      accessToken: undefined,
+      resolveLinks: undefined,
+      resolveRelations: undefined,
+      dataSources: {},
+      stories: {
+        article: { typeName: 'Article' }
+      }
+    }
+  }
 
-module.exports = function (api, options) {
-  var Storyblok = new StoryblokClient({
-    accessToken: options.queryParams.token
-  })
+  constructor (api, options) {
+    const StoryblokClient = require('storyblok-js-client')
 
-  api.loadSource(async store => {
-    const { data } = await Storyblok.get('cdn/stories/', options.queryParams)
-
-    const contentType = store.addContentType({
-      typeName: options.typeName
+    this.options = options
+    this.client = new StoryblokClient({
+      accessToken: options.accessToken
     })
 
-    for (const item of data.stories) {
-      contentType.addNode({
-        id: item.id,
-        slug: item.slug,
-        fields: {
-          published: item.published_at,
-          created: item.created_at,
-          ...item.content
-        }
+    api.loadSource(async store => {
+      await this.loadStories(store)
+      await this.loadDataSources(store)
+    })
+  }
+
+  async loadStories (store) {
+    const { stories, resolveRelations, resolveLinks } = this.options
+
+    for (const key in stories) {
+      const config = typeof stories[key] === 'string'
+        ? { typeName: stories[key] }
+        : stories[key]
+
+      if (!config.typeName) {
+        throw new Error(`No typeName defined for ${key}.`)
+      }
+
+      const collection = store.addContentType(config.typeName)
+
+      const { data } = await this.client.get('cdn/stories', {
+        resolve_links: resolveRelations || config.resolveLinks || 1,
+        resolve_relations: resolveLinks || config.resolveRelations,
+        filter_query: {
+          component: {
+            in: key
+          }
+        },
+        ...config.args
       })
+
+      for (const story of data.stories) {
+        collection.addNode({
+          id: String(story.id),
+          title: story.content.title || story.title,
+          slug: story.slug,
+          date: story.published_at,
+          fields: story.content
+        })
+      }
     }
-  })
+  }
+
+  async loadDataSources (store) {
+    for (const key in this.options.dataSources) {
+      const config = this.options.dataSources[key]
+
+      const res = await this.client.get('cdn/datasource_entries', {
+        dimension: config.dimension,
+        datasource: key,
+        ...config.args
+      })
+
+      const data = res.data.datasource_entries.reduce((acc, entry) => {
+        const { name, ...value } = entry
+        acc[name] = value
+        return acc
+      }, {})
+
+      store.addMetaData(config.fieldName || key, data)
+    }
+  }
 }
+
+module.exports = StoryblokSource
