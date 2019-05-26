d99ba55e6ccf5eb5654c5ebee5c0b703d782606a
Robert Axelsen <rob@rob.ee>
2019-05-26 15:03:18 +0200

    Fix linting errors

--- packages/source-airtable/index.js
+++ packages/source-airtable/index.js
@@ -1,26 +1,23 @@
-const Airtable = require('airtable');
+const Airtable = require('airtable')
 
 module.exports = function (api, options) {
-    const base = new Airtable({apiKey: options.apiKey}).base(options.base);
-    //const table = options.table
-    //const typeName = options.typeName
-    //const route = options.route
+  const base = new Airtable({apiKey: options.apiKey}).base(options.base)
 
-    api.loadSource(async store => {
-        const contentType = store.addContentType({
-            typeName: options.typeName,
-            route: `/${options.route}/:id`
-        });
+  api.loadSource(async store => {
+    const contentType = store.addContentType({
+      typeName: options.typeName,
+      route: `/${options.route}/:id`
+    })
 
-        await base(options.table).select().eachPage((records, fetchNextPage) => {
-            records.forEach((record) => {
-                const item = record._rawJson;
-                contentType.addNode({
-                    id: item.id,
-                    ...item.fields
-                });
-            });
-            fetchNextPage();
-        });
-    });
-};
+    await base(options.table).select().eachPage((records, fetchNextPage) => {
+      records.forEach((record) => {
+        const item = record._rawJson
+        contentType.addNode({
+            id: item.id,
+            ...item.fields
+        })
+      })
+      fetchNextPage()
+    })
+  })
+}
