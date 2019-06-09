c41c437dddd3318f26f1a6d03c58ead56ab3f261
Dominique Dutra <ddutra@fibersals.com.br>
2019-06-09 15:32:39 -0300

    Do not process images that already exist on outDir

--- gridsome/lib/workers/image-processor.js
+++ gridsome/lib/workers/image-processor.js
@@ -20,75 +20,76 @@ exports.processImage = async function ({
   if (cachePath && await fs.exists(cachePath)) {
     return fs.copy(cachePath, destPath)
   }
+  if (!fs.existsSync(destPath)) {
+    const { ext } = path.parse(filePath)
+    let buffer = await fs.readFile(filePath)
 
-  const { ext } = path.parse(filePath)
-  let buffer = await fs.readFile(filePath)
-
-  if (['.png', '.jpeg', '.jpg', '.webp'].includes(ext)) {
-    const config = {
-      pngCompressionLevel: parseInt(options.pngCompressionLevel, 10) || 9,
-      quality: parseInt(options.quality, 10) || 75,
-      width: parseInt(options.width, 10),
-      height: parseInt(options.height, 10),
-      jpegProgressive: true
-    }
-
-    const plugins = []
-    let pipeline = sharp(buffer)
-
-    if (config.width && config.width <= size.width) {
-      const ratio = size.height / size.width
-      const height = Math.round(config.width * ratio)
-      const resizeOptions = {}
-
-      if (options.fit) resizeOptions.fit = sharp.fit[options.fit]
-      if (options.position) resizeOptions.position = sharp.position[options.position]
-      if (options.background && colorString.get(options.background)) {
-        resizeOptions.background = options.background
-      } else if (backgroundColor) {
-        resizeOptions.background = backgroundColor
+    if (['.png', '.jpeg', '.jpg', '.webp'].includes(ext)) {
+      const config = {
+        pngCompressionLevel: parseInt(options.pngCompressionLevel, 10) || 9,
+        quality: parseInt(options.quality, 10) || 75,
+        width: parseInt(options.width, 10),
+        height: parseInt(options.height, 10),
+        jpegProgressive: true
       }
 
-      pipeline = pipeline.resize(config.width, height, resizeOptions)
+      const plugins = []
+      let pipeline = sharp(buffer)
+
+      if (config.width && config.width <= size.width) {
+        const ratio = size.height / size.width
+        const height = Math.round(config.width * ratio)
+        const resizeOptions = {}
+
+        if (options.fit) resizeOptions.fit = sharp.fit[options.fit]
+        if (options.position) resizeOptions.position = sharp.position[options.position]
+        if (options.background && colorString.get(options.background)) {
+          resizeOptions.background = options.background
+        } else if (backgroundColor) {
+          resizeOptions.background = backgroundColor
+        }
+
+        pipeline = pipeline.resize(config.width, height, resizeOptions)
+      }
+
+      if (/\.png$/.test(ext)) {
+        const quality = config.quality / 100
+
+        pipeline = pipeline.png({
+          compressionLevel: config.pngCompressionLevel,
+          adaptiveFiltering: false
+        })
+        plugins.push(imageminPngquant({
+          quality: [quality, quality]
+        }))
+      }
+
+      if (/\.jpe?g$/.test(ext)) {
+        pipeline = pipeline.jpeg({
+          progressive: config.jpegProgressive,
+          quality: config.quality
+        })
+        plugins.push(imageminMozjpeg({
+          progressive: config.jpegProgressive,
+          quality: config.quality
+        }))
+      }
+
+      if (/\.webp$/.test(ext)) {
+        pipeline = pipeline.webp({
+          quality: config.quality
+        })
+        plugins.push(imageminWebp({
+          quality: config.quality
+        }))
+      }
+
+      buffer = await pipeline.toBuffer()
+      buffer = await imagemin.buffer(buffer, { plugins })
     }
 
-    if (/\.png$/.test(ext)) {
-      const quality = config.quality / 100
-
-      pipeline = pipeline.png({
-        compressionLevel: config.pngCompressionLevel,
-        adaptiveFiltering: false
-      })
-      plugins.push(imageminPngquant({
-        quality: [quality, quality]
-      }))
-    }
-
-    if (/\.jpe?g$/.test(ext)) {
-      pipeline = pipeline.jpeg({
-        progressive: config.jpegProgressive,
-        quality: config.quality
-      })
-      plugins.push(imageminMozjpeg({
-        progressive: config.jpegProgressive,
-        quality: config.quality
-      }))
-    }
-
-    if (/\.webp$/.test(ext)) {
-      pipeline = pipeline.webp({
-        quality: config.quality
-      })
-      plugins.push(imageminWebp({
-        quality: config.quality
-      }))
-    }
-
-    buffer = await pipeline.toBuffer()
-    buffer = await imagemin.buffer(buffer, { plugins })
+    await fs.outputFile(destPath, buffer)
   }
-
-  await fs.outputFile(destPath, buffer)
 }
 
 exports.process = async function ({ queue, cacheDir, backgroundColor }) {
