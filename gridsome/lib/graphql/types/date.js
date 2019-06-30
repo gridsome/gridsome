375765f87b7f1cf23fe53e8d1988eba7fe51146f
Gerard Lamusse <>
2019-06-30 12:09:07 +0200

    Return null if date value is null or falsy

--- gridsome/lib/graphql/types/date.js
+++ gridsome/lib/graphql/types/date.js
@@ -24,7 +24,7 @@ exports.dateType = {
     locale: { type: GraphQLString, description: 'Locale' }
   },
   resolve: (obj, args, context, { fieldName }) => {
-    return formatDate(obj[fieldName], args)
+    return obj[fieldName] ? formatDate(obj[fieldName], args) : null
   }
 }
 
@@ -33,7 +33,7 @@ exports.dateTypeField = {
   ...exports.dateType,
   resolve: (obj, args, context, info) => {
     const value = fieldResolver(obj, args, context, info)
-    return formatDate(value, args)
+    return value ? formatDate(value, args) : null
   }
 }
 
