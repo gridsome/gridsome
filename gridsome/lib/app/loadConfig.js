c612f8e392f9a8b963a46e095228bea3b6550a71
Sigit Prabowo <sigit.prabowo@go-jek.com>
2019-07-13 17:33:58 +0700

    fix order default size and src favicon

--- gridsome/lib/app/loadConfig.js
+++ gridsome/lib/app/loadConfig.js
@@ -271,15 +271,15 @@ function normalizeIconsConfig (config = {}) {
   res.favicon = typeof icon.favicon === 'string'
     ? { src: icon.favicon, sizes: faviconSizes }
     : Object.assign({}, icon.favicon, {
-      sizes: (icon.favicon && icon.favicon.sizes) ? icon.favicon.sizes : faviconSizes,
-      src: (icon.favicon && icon.favicon.src) ? icon.favicon.src : defaultIcon
+      sizes: !(icon.favicon && icon.favicon.sizes) ? faviconSizes : icon.favicon.sizes,
+      src: !(icon.favicon && icon.favicon.src) ? defaultIcon : icon.favicon.src
     })
 
   res.touchicon = typeof icon.touchicon === 'string'
     ? { src: icon.touchicon, sizes: touchiconSizes, precomposed: false }
     : Object.assign({}, icon.touchicon, {
-      sizes: (icon.touchicon && icon.touchicon.sizes) ? icon.touchicon.sizes : touchiconSizes,
-      src: res.favicon.src,
+      sizes: !(icon.touchicon && icon.touchicon.sizes) ? touchiconSizes : icon.touchicon.sizes,
+      src: !(icon.touchicon && icon.touchicon.src) ? res.favicon.src : icon.touchicon.src,
       precomposed: false
     })
 
