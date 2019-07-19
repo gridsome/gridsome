73a4c388336c1467b0b1eb0de5a51286b4f73c47
Sigit Prabowo <sgt@prbw.net>
2019-07-19 17:43:20 +0700

    fix(config): load custom favicon config (#526)

--- gridsome/lib/app/loadConfig.js
+++ gridsome/lib/app/loadConfig.js
@@ -270,18 +270,11 @@ function normalizeIconsConfig (config = {}) {
 
   res.favicon = typeof icon.favicon === 'string'
     ? { src: icon.favicon, sizes: faviconSizes }
-    : Object.assign({}, icon.favicon, {
-      sizes: faviconSizes,
-      src: defaultIcon
-    })
+    : Object.assign({}, { src: defaultIcon, sizes: faviconSizes }, icon.favicon)
 
   res.touchicon = typeof icon.touchicon === 'string'
     ? { src: icon.touchicon, sizes: touchiconSizes, precomposed: false }
-    : Object.assign({}, icon.touchicon, {
-      sizes: touchiconSizes,
-      src: res.favicon.src,
-      precomposed: false
-    })
+    : Object.assign({}, { src: res.favicon.src, sizes: touchiconSizes, precomposed: false }, icon.touchicon)
 
   return res
 }
