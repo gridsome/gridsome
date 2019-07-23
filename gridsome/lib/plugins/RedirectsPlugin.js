3ca30fc1e69668803b434ab3242ba4afd5db8c08
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-07-23 12:49:08 +0200

    generate redirect routes

--- gridsome/lib/plugins/RedirectsPlugin.js
+++ gridsome/lib/plugins/RedirectsPlugin.js
@@ -3,32 +3,24 @@ const slash = require('slash')
 
 class RedirectsPlugin {
   constructor ({ _app: { hooks, config } }) {
-    const { outDir, publicPath, pathPrefix, redirects = [] } = config
+    const { outDir, redirects = [] } = config
 
     hooks.redirects.tap('RedirectsPlugin', (res, renderQueue) => {
       for (const entry of renderQueue) {
         const relative = path.relative(outDir, entry.htmlOutput)
-        const dirname = path.dirname(relative)
-        const url = dirname === '.' ? '/' : `/${slash(dirname)}`
+        const dirname = slash(path.dirname(relative))
+        const url = dirname === '.' ? '/' : `/${dirname}`
 
         if (entry.path !== '/' && entry.path !== url) {
           res.push({
-            from: pathPrefix + entry.path,
-            to: publicPath + relative,
+            from: entry.path,
+            to: `/${slash(relative)}`,
             status: 200
           })
         }
       }
 
-      for (const entry of redirects) {
-        res.push({
-          from: pathPrefix + entry.from,
-          to: publicPath + entry.to,
-          status: entry.status || 301
-        })
-      }
-
-      return res
+      return res.concat(redirects)
     })
   }
 }
