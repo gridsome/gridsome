a5db71138a89914a0d821952d0d067752456f14f
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-09-02 23:38:04 +0200

    minor update

--- gridsome/lib/graphql/utils.js
+++ gridsome/lib/graphql/utils.js
@@ -22,13 +22,6 @@ exports.createQueryVariables = function (path, variables, currentPage = undefine
   })
 }
 
-exports.createQueryVariables = function (path, variables, currentPage = undefined) {
-  return Object.assign({}, variables, {
-    page: currentPage,
-    __path: path
-  })
-}
-
 exports.is32BitInt = function (x) {
   return (x | 0) === x
 }
