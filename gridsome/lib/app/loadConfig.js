1ca774f4880715dad3d489a503ad4db43e5964ac
Ben Sheedy <ben.sheedy@digitalrisks.co.uk>
2019-11-18 13:45:51 +0000

    fix(g-image): set defaultBlur back to 40

--- gridsome/lib/app/loadConfig.js
+++ gridsome/lib/app/loadConfig.js
@@ -113,7 +113,7 @@ module.exports = (context, options = {}) => {
 
   config.images = { ...localConfig.images }
 
-  config.images.defaultBlur = config.images.defaultBlur || 20
+  config.images.defaultBlur = config.images.defaultBlur || 40
 
   if (!colorString.get(config.images.backgroundColor || '')) {
     config.images.backgroundColor = null
