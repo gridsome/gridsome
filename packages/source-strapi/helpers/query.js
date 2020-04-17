8be1ab56e233341816cb38926dc441c52f96d7f8
Cornelius Müller <muellercornelius1@gmail.com>
2020-04-17 11:47:13 +0200

    Fix eslint errors

--- packages/source-strapi/helpers/query.js
+++ packages/source-strapi/helpers/query.js
@@ -1,14 +1,13 @@
 const axios = require('axios')
 const pluralize = require('pluralize')
 
-module.exports = async ({ apiURL, resourceName, jwtToken, queryLimit, isSingleType}) => {
+module.exports = async ({ apiURL, resourceName, jwtToken, queryLimit, isSingleType }) => {
   let resource
-  if(isSingleType) {
+  if (isSingleType) {
     resource = resourceName
   } else {
     resource = pluralize(resourceName)
   }
-  
 
   // Define API endpoint.
   const apiEndpoint = `${apiURL}/${resource}?_limit=${queryLimit}`
