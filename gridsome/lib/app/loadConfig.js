022901e38c7a8632987a41a6b2b6625f7181ea2e
lulzneko <lulzneko@riotz.works>
2020-04-19 17:44:02 +0900

    fix(sass-loader): improve default options (#750)

--- gridsome/lib/app/loadConfig.js
+++ gridsome/lib/app/loadConfig.js
@@ -34,7 +34,9 @@ module.exports = (context, options = {}) => {
     split: false,
     loaderOptions: {
       sass: {
-        indentedSyntax: true
+        sassOptions: {
+          indentedSyntax: true
+        }
       },
       stylus: {
         preferPathResolver: 'webpack'
