7ffd72cf11483c7e038a70ca172d96db235f321c
marcvangend <info@marcvangend.nl>
2020-05-23 20:50:19 +0200

    avoid nullish coalescing operator to support more node.js versions

--- packages/plugin-gtag/gridsome.server.js
+++ packages/plugin-gtag/gridsome.server.js
@@ -1,5 +1,7 @@
 module.exports = (api, options) => {
   // If enabled is not overridden from the plugin configuration, set a default.
-  options.enabled = options.enabled ?? process.env.NODE_ENV === 'production'
+  options.enabled = (options.enabled === undefined)
+    ? process.env.NODE_ENV === 'production'
+    : options.enabled
   api.setClientOptions(options)
 }
