0eed2e038c9211b309e14157fbb27eeda272cd99
orblazer <orblazer.minecraft@gmail.com>
2020-06-30 00:06:36 +0200

    fix(filter): add missing ref exists functions

--- gridsome/lib/graphql/filters/query.js
+++ gridsome/lib/graphql/filters/query.js
@@ -5,14 +5,16 @@ const listRefOpsMap = {
   '$in': '$refListIn',
   '$nin': '$refListNin',
   '$eq': '$refListEq',
-  '$ne': '$refListNe'
+  '$ne': '$refListNe',
+  '$exists': '$refListExists'
 }
 
 const refOpsMap = {
   '$in': '$refIn',
   '$nin': '$refNin',
   '$eq': '$refEq',
-  '$ne': '$refNe'
+  '$ne': '$refNe',
+  '$exists': '$refExists'
 }
 
 function toFilterArgs (filter, typeComposer, currentKey = '') {
