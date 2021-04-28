8f457b31f841b8db03a35f2980f61439517b12c4
jamesgeorge007 <jamesgeorge998001@gmail.com>
2021-04-28 18:25:14 +0530

    chore(cli): return with an exit code 1 for a non-empty target directory

--- packages/cli/__tests__/config.spec.js
+++ packages/cli/__tests__/config.spec.js
@@ -1,13 +1,13 @@
 const runCLI = require('./utils/helpers')
 
 test('warns if unkown option name', async () => {
-  const { stdout } = await runCLI(['config', '--set', 'asdf'], __dirname)
+  const { stdout } = await runCLI(['config', '--set', 'asdf'], { cwd: __dirname })
 
   expect(stdout).toBe('Unknown option: asdf')
 })
 
 test('warns if unkown package manager', async () => {
-  const { stdout } = await runCLI(['config', '--set', 'packageManager', 'asdf'], __dirname)
+  const { stdout } = await runCLI(['config', '--set', 'packageManager', 'asdf'], { cwd: __dirname })
 
   expect(stdout).toMatch('Unsupported package manager: asdf')
   expect(stdout).toMatch('gridsome config --set packageManager none')
