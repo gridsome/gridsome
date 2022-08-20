bc04d02c7c46ee28b728724c49f7992fffa2224c
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2022-08-20 04:41:23 +0200

    feat: `useMetaInfo`

--- gridsome/app/index.js
+++ gridsome/app/index.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue, { getCurrentInstance } from 'vue'
 
 export { default as Vue } from 'vue'
 export { default as Link } from './components/Link'
@@ -9,4 +9,15 @@ export { url } from './utils/helpers'
 
 export function fetch(path) {
   return Vue.prototype.$fetch(path)
+}
+
+export function useMeta(meta) {
+  const instance = getCurrentInstance()
+  if (instance.proxy.$options.metaInfo) {
+    throw new Error(
+      `\`metaInfo\` has already been defined in the normal <string> tag.`
+    )
+  }
+  instance.proxy.$options.metaInfo = meta
+  instance.proxy._vueMeta = true
 }
